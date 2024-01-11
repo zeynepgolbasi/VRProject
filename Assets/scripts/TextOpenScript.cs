d4db5e37bdc6b0ecb5fab004fff6e48d87c21cb0
zeynepgolbasi <zeynepcansu2001@hotmail.com>
2024-01-11 20:30:56 +0300

    a

--- Assets/scripts/TextOpenScript.cs
+++ Assets/scripts/TextOpenScript.cs
@@ -2,21 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
 
-public class TextOpenScript : MonoBehaviour
-{
-    public Text textToShow;
-    public void OpenText()
+    // Bu fonksiyon, butona her týklandýðýnda çaðrýlýr.
+    public class TextOpenScript : MonoBehaviour
     {
-        // Metin kutusunu görünür yap.
-        textToShow.gameObject.SetActive(!textToShow.gameObject.activeSelf);
+        public TextMeshProUGUI textToShow;
 
-        // Eðer metin kutusu görünür deðilse metni kapat
-        if (!textToShow.gameObject.activeSelf)
+        // Bu fonksiyon, butona her týklandýðýnda çaðrýlýr.
+        public void ToggleTextVisibility()
         {
-            textToShow.text = "";
+            // Text Mesh Pro öðesinin görünürlük durumunu tersine çevir.
+            if (textToShow.gameObject.activeSelf)
+            {
+                // Eðer metni görünür yapýyorsa, gizle.
+                textToShow.gameObject.SetActive(false);
+            }
+            else
+            {
+                // Eðer metni gizli yapýyorsa, görünür yap.
+                textToShow.gameObject.SetActive(true);
+
+            }
         }
-       
     }
-}
+
+
 
