636fb9d7ad1b6d78842554dbafb8f8a0d8f298c9
IREMTARGıT\irem turgut <iremt.151@gmail.com>
2024-01-11 18:27:20 +0300

    duman eklendi

---
+++ Assets/scripts/ParticleController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleController : MonoBehaviour
+{
+   public ParticleSystem particleSystemToStart; // Unity Editor üzerinde atanacak
+
+    private bool particleSystemStarted = false;
+
+   public void Start()
+    {
+        // Ýstediðiniz bir yerde bu metodu çaðýrabilirsiniz
+        PlayParticleSystem();
+    }
+
+    void PlayParticleSystem()
+    {
+        // Butona týklandýðýnda çalýþacak kod
+        if (!particleSystemStarted)
+        {
+            // Particle System'i baþlat
+            if (particleSystemToStart != null)
+            {
+                particleSystemToStart.Play();
+                particleSystemStarted = true;
+
+                // Invoke ile belirli bir süre sonra StopParticleSystems metodu çaðrýlýr
+                Invoke("StopParticleSystems", particleSystemToStart.main.duration);
+            }
+        }
+    }
+
+    void StopParticleSystems()
+    {
+        // Particle System'i durdur
+        if (particleSystemToStart != null)
+        {
+            particleSystemToStart.Stop();
+            particleSystemStarted = false;
+        }
+    }
+}
